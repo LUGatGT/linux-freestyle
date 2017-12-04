b9e512fa203a74acd61734445bec143828e462cd
User <zipper@jacobzipper.com>
2017-12-04 18:36:10 -0500

    click to get id

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', distros:['ubuntu', 'fedora', 'test'] });
+  res.render('index', { title: 'Express', distros:['ubuntu', 'fedora', 'arch', 'slackware', 'gentoo', 'debian', 'opensuse', 'dragonos', 'mint', 'centos'] });
 });
 
 module.exports = router;
