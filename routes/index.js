a3a5d1bbbcd22eb1f830dc56152de88ce575b86c
User <zipper@jacobzipper.com>
2017-12-04 20:54:49 -0500

    borders, center view, and usb watching server side

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var Inotify = require('inotify').Inotify;
+var inotify = new Inotify();
 
+var devDir = {
+  path:'/dev/', // <--- change this for a valid directory in your machine.
+  watch_for: Inotify.IN_CREATE | Inotify.IN_DELETE,
+  callback: function(event) {
+    console.log(event.name);
+  }
+};
+
+var devDescriptor = inotify.addWatch(devDir);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', distros:['ubuntu', 'fedora', 'arch', 'slackware', 'gentoo', 'debian', 'opensuse', 'dragonos', 'mint', 'centos'] });
 });
-
+router.post('/install', function(req, res, next) {
+  res.json({ 'error': false });
+});
 module.exports = router;
