1102d9d8e06a83fd3c4324fec1cc1fcbe14eab95
Collin Richards <richardscollin@gatech.edu>
2018-01-13 01:35:23 -0500

    Add status information from dd
    Status info can currently be accessed by sending
    a GET request to /status. I've changed the subprocess
    mechanism to longer use python. SocketIO is no longer
    required, which I'll remove completely in the next commit.

--- ddhelper.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-from socketIO_client import SocketIO
-
-socketIO = SocketIO('127.0.0.1', 4000)
-os.system("sh exec.sh " + sys.argv[1] + " " + sys.argv[2] + " " + sys.argv[3])
-socketIO.emit('data', sys.argv[3])
-print 'done dd'
