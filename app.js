f8286769d309bb4427a5ea5117dcdc0429e74dce
Collin Richards <richardscollin@gatech.edu>
2018-01-21 06:00:44 -0500

    Remove unnecessary code

--- app.js
+++ app.js
@@ -2,38 +2,25 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 
 var app = express();
-var http = require('http').Server(app);
-
-http.listen(4000, function(){
-  console.log('listening on *:4000');
-});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -51,4 +38,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-module.exports.http = http;
